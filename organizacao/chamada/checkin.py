fbb2ce570307d13d7cbc53f2b157ec2f82df25bb
alex <alex.pereira@planejamento.gov.br>
2020-08-08 17:50:41 -0300

    fiz um script pra contabilizar a chamada

--- organizacao/chamada/checkin.py
+++ organizacao/chamada/checkin.py
@@ -1,11 +1,19 @@
-import datetime
-import mpu
+import math
+from random import shuffle
 from chamada.auth_data import user, password
 from chamada.scrapy import scrapy_students
 import pandas as pd
 
 history_pickle = './data/history.pickle'
 
+def get_students():
+    try:
+        history = pd.read_pickle(history_pickle)
+    except FileNotFoundError as e:
+        pass
+        return None
+    students = history['nome'].to_list()
+    return students
 
 def add_checking(exclude_list):
     try:
@@ -18,13 +26,35 @@ def add_checking(exclude_list):
         history = pd.DataFrame(students)
     else:
         current = pd.DataFrame(students)
-        history = history.merge(current, on='nome')
+        history = history.merge(current, on='nome', how='right')
     print(history.to_markdown())
     history.to_pickle(history_pickle)
 
+def create_groups(students, groups, group_length):
+    N = len(students)
+    groups_rep = [[groups[g]] * group_length for g in range(math.ceil(N / group_length))]
+    flat_groups = [item for sublist in groups_rep for item in sublist]
+    flat_groups = flat_groups[:N]
+    shuffle(students)
+    df = pd.DataFrame(index=flat_groups, data=students)
+    return df
 
-exclude_list = None #['Slackbot', 'alexlopespereira', 'Bruno Melo', 'Erick Muzart', 'stefanomozart', 'vronks']
-with open('./data/exclude_list') as f:
-  exclude_list = f.read().splitlines()
 
-add_checking(exclude_list)
+
+def run():
+    exclude_list = None
+    groups = None
+
+    with open('./data/exclude_list') as f:
+      exclude_list = f.read().splitlines()
+
+    with open('./data/groups') as f:
+      groups = f.read().splitlines()
+
+
+    add_checking(exclude_list)
+    students = get_students()
+    df = create_groups(students, groups, 4)
+    print(df.to_markdown())
+
+run()
