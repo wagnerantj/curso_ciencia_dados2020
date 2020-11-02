d4ab6ee593b50c0acaaabf398f6b18a5bde99109
Alex Lopes Pereira <alexlopespereira@gmail.com>
2020-11-02 19:30:04 -0300

    Melhorei

--- scrapy/sei.py
+++ scrapy/sei.py
@@ -5,18 +5,18 @@ import re
 from selenium import webdriver
 
 login = os.environ.get('LOGIN') or 'usuario'
-passowrd = os.environ.get('PASSWORD') or 'minhasenha'
+password = os.environ.get('PASSWORD') or 'minhasenha'
 
 url = 'https://sei.fazenda.gov.br/'
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get(url)
-input_login = driver.find_element_by_xpath('//input[@id="txtUsuario"]')
+input_login = driver.find_element_by_xpath('//*[@id="txtUsuario"]')
 input_login.send_keys(login)
-input_senha = driver.find_element_by_xpath('//input[@id="pwdSenha"]')
+input_senha = driver.find_element_by_xpath('//*[@id="pwdSenha"]')
 input_senha.send_keys(passowrd)
 option = driver.find_element_by_xpath('//option[@value="0"]')
 option.click()
-button_acessar = driver.find_element_by_xpath('//button[@id="sbmLogin"]')
+button_acessar = driver.find_element_by_xpath('//*[@id="sbmLogin"]')
 button_acessar.click()
 print(driver)
 driver.close()
